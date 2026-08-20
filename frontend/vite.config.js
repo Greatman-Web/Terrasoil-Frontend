49e8fa87c2a699eb3caee498c38a5aa8c119b2c9
Greatman-web <g.amadimati.25@abdn.ac.uk>
2026-08-20 17:29:25 +0100

    Installage of PWA features and Plugins.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      developmentOptions: {
+        enabled: true
+      },
+
+      manifest: {
+        name: 'Farmily',
+        short_name: 'Farmily',
+        description: 'Smart Soil Health Monitoring Platform',
+
+        theme_color: '#8bb82e',
+        background_color: '#f5f0e6',
+
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ]
+      },
+    }),
+  ],
 })
